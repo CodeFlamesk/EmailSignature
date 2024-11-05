28d0059e0a792bd2786e234ec2b94c3dba60a1ee
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-11-05 12:42:50 +0100

    style upgrade

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,25 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.0.5:8081');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
+
+app.listen(8080);
+
+async function getCars() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/photos')
+    let content = await response.json()
+    console.log(content.splice(0, 3))
+}
+getCars()
